8c76a26983b8dbe7ad2ca7f6fe4c7331de8f910d
rocketmike12 <102810533+rocketmike12@users.noreply.github.com>
2022-08-23 12:28:54 +0300

    fixed generating x and y, finished main.py

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
-from xy import generate_xy
-from matplotlib import pyplot
+from xy import generate_xy as xygen
+import random
+from matplotlib import pyplot as plt
 
+xy_list = xygen.generate_xy(int(input('Enter a\n')), int(input('Enter b\n')))
 
+plt.plot(xy_list[0], xy_list[1], label='x and y')
+plt.title('x and y plot')
+plt.xlabel('x label')
+plt.ylabel('y label')
+plt.legend()
+plt.show()
--- xy/generate_xy.py
+++ xy/generate_xy.py
@@ -1,7 +1,5 @@
 def generate_xy(a: int, b: int):
     import random
-    if a < b:
-        a, b = b, a
-    x = random.randint(a, b)
-    y = random.randint(a, b)
+    x = [0, random.randint(a, b), random.randint(a, b), random.randint(a, b), random.randint(a, b)]
+    y = [0, random.randint(a, b), random.randint(a, b), random.randint(a, b), random.randint(a, b)]
     return [x, y]
